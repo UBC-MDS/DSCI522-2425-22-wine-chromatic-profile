309f19c49e1c8b9791979a9cb7e46d9e0958835d
Adrian Leung <107446173+adrianl726@users.noreply.github.com>
2024-12-13 04:05:02 -0800

    evaluation function script + test (#69)
    * added test evaluation function and tests script
    
    * renamed test file
    
    * added pytest commands in tests/README
    
    * fix bugs so the test runs
    
    * edited README
    
    * edited README

---
+++ tests/test_evaluation.py
@@ -0,0 +1,91 @@
+# test_evaluation.py
+# author: Farhan Bin Faisal, Daria Khon, Adrian Leung, Zhiwei Zhang
+# date: 2024-12-12
+
+import pytest
+import pandas as pd
+import pickle
+from sklearn.linear_model import LogisticRegression
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.evaluation import evaluation
+
+# Create data to test
+X_test_data = {
+    'fixed_acidity': [7.4, 7.8, 7.4],
+    'volatile_acidity': [0.7, 0.88, 0.76],
+    'citric_acid': [0.0, 0.0, 0.04],
+    'residual_sugar': [1.9, 2.6, 2.3],
+    'chlorides': [0.076, 0.098, 0.092],
+    'free_sulfur_dioxide': [11.0, 25.0, 15.0],
+    'total_sulfur_dioxide': [34.0, 67.0, 54.0],
+    'density': [0.9978, 0.9968, 0.9970],
+    'pH': [3.51, 3.20, 3.26],
+    'sulphates': [0.56, 0.68, 0.65],
+    'alcohol': [9.4, 9.8, 9.8],
+    'quality': [5, 5, 5]
+}
+X_test = pd.DataFrame(X_test_data)
+y_test = pd.Series(['red', 'red', 'white'])
+
+# Create a fitted model
+model = LogisticRegression(max_iter=1000, class_weight="balanced")
+model.fit(X_test, y_test)
+
+# temporaray plot_to, table_to paths
+table_to = "tables"
+plot_to = "plots"
+
+def test_saving():
+    """
+    This tests that the function test_evaluation saves the plots and tables in the right place
+    """
+    # Create temporary paths to save models
+    os.mkdir(table_to)
+    os.mkdir(plot_to)
+
+    evaluation(model, X_test, y_test, "red", table_to, plot_to)
+
+    table_path = 'tables/test_scores.csv'
+    con_plot_path = 'plots/confusion_matrix.png'
+    pr_plot_path = 'plots/pr_curve.png'
+
+    assert os.path.exists(table_path), f"Test scores results does not exist in the directory '{table_path}'"
+    assert os.path.exists(con_plot_path), f"Confusion matrix does not exist in the directory '{con_plot_path}'"
+    assert os.path.exists(pr_plot_path), f"PR curve does not exist in the directory '{pr_plot_path}'"
+
+    os.system("rm -rf tables/")
+    os.system("rm -rf plots/")
+
+
+def test_table():
+    """
+    This tests the table output saved in the table path.
+    """
+    os.mkdir(table_to)
+    os.mkdir(plot_to)
+
+    evaluation(model, X_test, y_test, "red", table_to, plot_to)
+    table_path =  'tables/test_scores.csv'
+    test_scores = pd.read_csv(table_path)
+
+    # test if the output test scores dataframe has only one row
+    assert test_scores.shape[0] == 1, "The test scores dataframe does not contain one row."
+    # test if the output test scores dataframe has 4 columns
+    assert test_scores.shape[1] == 4, "The test scores dataframe does not contain four columns."
+    # test the output test scores column names
+    assert list(test_scores.columns) == ["accuracy", "precision", "recall", "F1 score"], "The test scores dataframe does not contain the correct columns names."
+    # test the scores are floats
+    assert isinstance(test_scores.iloc[0, 0], float), "Accuracy score is not a float."
+    assert isinstance(test_scores.iloc[0, 1], float), "Precision score is not a float."
+    assert isinstance(test_scores.iloc[0, 2], float), "Recall score is not a float."
+    assert isinstance(test_scores.iloc[0, 3], float), "F1 score is not a float."
+    # check the range of the scores
+    assert 0 <= test_scores.iloc[0, 0] <= 1, "Accuracy is not within valid range."
+    assert 0 <= test_scores.iloc[0, 1] <= 1, "Precision is not within valid range."
+    assert 0 <= test_scores.iloc[0, 2] <= 1, "Recall is not within valid range."
+    assert 0 <= test_scores.iloc[0, 3] <= 1, "F1 is not within valid range."
+
+    os.system("rm -rf tables/")
+    os.system("rm -rf plots/")
